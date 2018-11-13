1eecb0059c95440218e8adc2dc9ca0adcba15b2b
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2018-11-13 13:10:05 +0100

    init unit tests with mocha

---
+++ tests/unittests/LoadStyler_unitTests.js
@@ -0,0 +1,48 @@
+
+
+//const Styler = require('../../src/pageone/js/LoadStyler');  //TODO da stimmt was nicht!
+
+const assert = require('assert');
+
+
+
+describe('Basic Mocha Tests for the LoadStyler', function() {
+  
+  before(function() {
+
+    //const LoadStyler = require('../../src/pageone/js/LoadStyler');
+
+    //let Loader = new LoadStyler();
+
+  });
+  
+
+  describe('Given Parameter leads to expected behaviour test',function() {
+
+    it('',function() {
+
+
+
+    });
+
+  });
+
+  describe('Return value has expected type', function() {
+
+  });
+
+  describe('',function() {
+
+  });
+
+  describe('Throws error as expected', function() {
+
+  });
+
+
+
+
+
+
+});
+
