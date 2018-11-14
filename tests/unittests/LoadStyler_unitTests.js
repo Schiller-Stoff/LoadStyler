531d792f4c1ae0cd35e8c57d29e07bd8b8844ba7
Stoff_Sebi <34453177+oldschmetterhand@users.noreply.github.com>
2018-11-14 12:15:03 +0100

    implemented JSDOM for the unittests

--- tests/unittests/LoadStyler_unitTests.js
+++ tests/unittests/LoadStyler_unitTests.js
@@ -1,12 +1,21 @@
 
-//import LoadStyler from '../../src/pageone/js/LoadStyler.js';
-
-//const Styler = new LoadStyler([]);
+//import LoadStyler from '../../src/pageone/js/LoadStyler.js'; ---> ES6 import statements don't work!!!
 
 const LoadStyler = require('../../src/pageone/js/LoadStyler');  //TODO da stimmt was nicht!
-const styler = new LoadStyler([]);
 const assert = require('assert');
 
+//applying JSDOM
+const jsdom = require("jsdom");
+const { JSDOM } = jsdom;
+const dom = new JSDOM('<!DOCTYPE html><html><head></head><body></body></html>');
+global.window = dom.window;
+global.document = dom.window.document;
+
+//adding jquery
+global.jquery = require('jquery');
+global.$ = global.jquery;
+
+//const $ = global.jquery = require('jquery')(window);
 
 
 describe('Basic Mocha Tests for the LoadStyler', function() {
@@ -22,10 +31,18 @@ describe('Basic Mocha Tests for the LoadStyler', function() {
 
     it('this.pageLeaveLinks: Internal class variable has expected value from above (an empty array)',function() {
 
+      const styler = new LoadStyler();
+
       let class_string = JSON.stringify(styler.pageLeaveLinks);
       let test_string = '[]';
 
       assert.equal(class_string, test_string);
+
+      //$('body').append($('<div id="nice"></div>'));
+
+      //console.log(document);
+      //console.log(document.body);
+      //console.log($('#nice').attr('id'));
 
     });
 
